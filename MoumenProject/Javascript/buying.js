92fd01f0d1d18cc8e60f79474c73efeb5fbb8612
MoumenJehad <141726988+MoumenJehad@users.noreply.github.com>
2025-09-05 13:33:12 +0300

    Delete MoumenProject directory

--- MoumenProject/Javascript/buying.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("input[type=submit]").addEventListener("click", function() {
-    validateForm();
-  });
-  
-  function validateForm() {
-    alert("تم تقديم النموذج بنجاح.");
-  }
-  
